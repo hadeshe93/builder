f9b7acc256bc6e784a1bfedc274dbadf5386ce3e
hadeshe93 <hadeshe93@gmail.com>
2023-01-13 12:56:04 +0800

    chore: 新增 @hadeshe93/eslint-config-hh 包

---
+++ packages/eslint-config-hh/src/index.ts
@@ -0,0 +1,35 @@
+// import { getNodeModulePaths, getRequireResolve } from './utils';
+// const requireResolve = getRequireResolve(getNodeModulePaths());
+
+export default {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    },
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: '*.ts',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./packages/**/tsconfig.json', './tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+};
