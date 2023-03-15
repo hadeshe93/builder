e938ae85600e24a9893bd82dea30db99972224f3
hadeshe93 <hadeshe93@gmail.com>
2023-03-15 10:37:14 +0800

    fix: builder-core 补充发布 assets 文件夹到仓库

---
+++ .changeset/old-cooks-sniff.md
@@ -0,0 +1,6 @@
+---
+"@hadeshe93/builder-core": patch
+"@hadeshe93/builder-vite": patch
+---
+
+builder-core 补充发布 assets 文件夹到仓库
--- packages/builder-vite/smoke-tests/index.test.ts
+++ packages/builder-vite/smoke-tests/index.test.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { BuilderConfig, ProjectConfig, formatBuilderConfig } from '@hadeshe93/builder-core';
 import BuilderVite from '../src/index';
 
-const projectPath = '/cbs/xcode/web-project-starter/packages/vite3-vue3';
+const projectPath = '/cbs/xcode/web-project-starter/packages/vite3-vue3/apps/app1';
 const pageName = 'demo1';
 const projectPagePath = path.resolve(projectPath, 'src', 'pages', pageName);
 const projectConfig: ProjectConfig = require(path.resolve(projectPagePath, 'project.config.js'));
