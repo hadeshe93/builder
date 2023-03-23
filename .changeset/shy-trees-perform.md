36da516196c53fdaa07c5e82bcbd1c4a78a2a2ee
hadeshe93 <hadeshe93@gmail.com>
2023-03-23 20:24:37 +0800

    fix: 优化 builder-webpack 的 publicPath 默认使用 pageName 下的路径

---
+++ .changeset/shy-trees-perform.md
@@ -0,0 +1,5 @@
+---
+"@hadeshe93/builder-webpack": patch
+---
+
+优化 builder-webpack 的 publicPath 默认使用 pageName 下的路径
--- packages/builder-webpack/src/mws/common-config-mw.ts
+++ packages/builder-webpack/src/mws/common-config-mw.ts
@@ -1,18 +1,23 @@
+import path from 'path';
 import WebpackChain from 'webpack-chain';
 import { getDevChainConfig } from '../wpconfig/dev.config'; 
 import { getProdChainConfig } from '../wpconfig/prod.config'; 
 import { PureBuilderConfig } from '../typings/index';
 import { ParamsGetWebpackChainConfigs } from '../typings/configs';
 
+const getDefaultPublicPath = (pageName: string) => path.join(path.sep, pageName, path.sep);
+
 export default function getCommonConfigMw(buildConfig: PureBuilderConfig) {
   const { mode, projectPath, pageName, projectConfig } = buildConfig;
   const { build } = projectConfig;
+  const { publicPath: rawPublicPath = '', devPort } = build;
+  const publicPath = rawPublicPath ? rawPublicPath : getDefaultPublicPath(pageName);
   const params: ParamsGetWebpackChainConfigs = {
     mode: mode as ParamsGetWebpackChainConfigs['mode'],
     projectPath,
     pageName,
-    publicPath: build.publicPath,
-    devPort: build.devPort,
+    publicPath,
+    devPort,
   };
   if (build.dllEntryMap) params.dllEntryMap = build.dllEntryMap;
   return function(chainConfig: WebpackChain): WebpackChain {
